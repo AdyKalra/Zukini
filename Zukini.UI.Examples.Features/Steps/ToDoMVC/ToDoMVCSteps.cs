d85658def9083ea51f7ff24ebac2273c5db18a36
Aditya Kalra <aditya_kalra@ymail.com>
2017-08-07 05:36:55 +1000

    added step defs for ToDoMVC

---
+++ Zukini.UI.Examples.Features/Steps/ToDoMVC/ToDoMVCSteps.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Zukini.UI.Examples.Features.Steps.ToDoMVC
+{
+    [Binding]
+    public class ToDoMVCSteps
+    {
+        [Given(@"I am on AngularJS page")]
+        public void GivenIAmOnAngularJSPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I choose to add ToDo Item")]
+        public void WhenIChooseToAddToDoItem()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I choose to edit content of First Item")]
+        public void WhenIChooseToEditContentOfFirstItem()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I choose to complete an Item")]
+        public void WhenIChooseToCompleteAnItem()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I choose to reactivate an completed Item")]
+        public void WhenIChooseToReactivateAnCompletedItem()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I choose to add second ToDO Item")]
+        public void WhenIChooseToAddSecondToDOItem()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I choose to mark all ToDo Items as Completed")]
+        public void WhenIChooseToMarkAllToDoItemsAsCompleted()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I choose to filter by Completed Items")]
+        public void WhenIChooseToFilterByCompletedItems()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I choose to click the close icon of first ToDo Item")]
+        public void WhenIChooseToClickTheCloseIconOfFirstToDoItem()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I choose to click the Clear Completed button")]
+        public void WhenIChooseToClickTheClearCompletedButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Item should be displayed")]
+        public void ThenItemShouldBeDisplayed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Item should be marked as completed")]
+        public void ThenItemShouldBeMarkedAsCompleted()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Item should be reactivated")]
+        public void ThenItemShouldBeReactivated()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Completed Items should be displayed")]
+        public void ThenCompletedItemsShouldBeDisplayed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"First ToDoItem should be cleared")]
+        public void ThenFirstToDoItemShouldBeCleared()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"All completed ToDo items should be cleared")]
+        public void ThenAllCompletedToDoItemsShouldBeCleared()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
